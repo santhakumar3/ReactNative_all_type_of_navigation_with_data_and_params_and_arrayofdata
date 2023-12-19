b5e55db868ce91aea988ee6e3d287d9ee4d9b14c
santhakumar <santhakumar3490@gmail.com>
2023-12-19 22:32:18 +0530

    Initial commit
    Generated by react-native@0.73.1

---
+++ android/app/src/main/java/com/all_types_of_navigation_with_pass_data/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.all_types_of_navigation_with_pass_data
+
+import com.facebook.react.ReactActivity
+import com.facebook.react.ReactActivityDelegate
+import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
+import com.facebook.react.defaults.DefaultReactActivityDelegate
+
+class MainActivity : ReactActivity() {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  override fun getMainComponentName(): String = "all_types_of_navigation_with_pass_data"
+
+  /**
+   * Returns the instance of the [ReactActivityDelegate]. We use [DefaultReactActivityDelegate]
+   * which allows you to enable New Architecture with a single boolean flags [fabricEnabled]
+   */
+  override fun createReactActivityDelegate(): ReactActivityDelegate =
+      DefaultReactActivityDelegate(this, mainComponentName, fabricEnabled)
+}
